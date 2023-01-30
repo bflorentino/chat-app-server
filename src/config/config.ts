306b15b98bc88966462ab4da91b9e8ad8826a657
Bryan <bryanmontero81@gmail.com>
2023-01-30 19:20:48 -0400

    Vercel config

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV !== "production"){
 const envVariables = {
     port: process.env.PORT,
     nodeEnv: process.env.NODE_ENV,
-    connectionString: process.env.CONNECTION_STRING_DEV,
+    connectionString: process.env.CONNECTION_STRING,
     connectionStringTest: process.env.CONNECTION_STRING_TEST,
     secretToken: process.env.SECRET_TOKEN,
     
